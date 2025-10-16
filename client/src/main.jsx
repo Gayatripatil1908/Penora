7a16c610033a3e1e5b2138074a292a7fd7e7290f
Gayatripatil1908 <gayatripatil9505@gmail.com>
2025-10-16 19:48:36 +0530

    route setup done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {BrowerRouter, Routes, Route} from 'react-router'
+import AllBlogs from './views/AllBlogs.jsx';
+import MyBlogs from './views/MyBlogs.jsx';
+import NewBlog from './views/NewBlog.jsx';
+import EditBlog from './views/EditBlog.jsx';
+import ReadBlog from './views/ReadBlog.jsx';
+
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowerRouter>
+  <Routes>
+    <Route path='/' element={<AllBlogs/>}/>
+    <Route path='/myBlogs' element={<MyBlogs/>}/>
+    <Route path='/new' element={<NewBlog/>}/>
+    <Route path='/edit/:id' element={<EditBlog/>}/>
+    <Route path='/blog/:slug' element={<ReadBlog/>}/>
+    <Route path='*' element={<h1 className='text-center'>404 Not</h1>}/>
+
+    
+  </Routes>
+
+  </BrowerRouter>
 )
