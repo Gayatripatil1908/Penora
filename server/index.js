b0f9e20986fc6e65d89206f496e158f3b78a0757
Gayatripatil1908 <gayatripatil9505@gmail.com>
2025-10-24 10:27:25 +0530

    new changes in server

--- server/index.js
+++ server/index.js
@@ -1,92 +1,90 @@
-import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import {
+  getBlogForSlug,
+  getBlogs,
+  patchPublishBlog,
+  postBlogs,
+  putBlogs,
+} from "./controllers/blog.js";
 import { postLogin, postSignup } from "./controllers/user.js";
-import { postBlogs, getBlogs, getBlogsForSlug } from "./controllers/blog.js";
-
+import Blog from "./models/Blog.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-let requestCount = 0;
+app.use(express.json());
+app.use(cors());
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        if (conn) {
-            console.log("MongoDB connected");
-        }
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
+      console.log("MongoDB connected");
     }
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
 };
 
-app.get("/api/request-count", (req, res) => {
-    res.json({requestCount});
-});
-
-app.use((req, res, next) => {
-    requestCount++;
-    console.log(`Request Count: ${requestCount}`);
-    next();
-});
-    
-
 app.get("/", (req, res) => {
-    res.json({sucess: true, message: "Server is working"});
-
+  res.json({
+    success: true,
+    message: "Server is up and running...",
+  });
 });
+
+const jwtCheck = (req, res, next) => {
+  req.user = null;
+
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(400).json({ message: "Authorization token missing" });
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid JWT Token" });
+  }
+};
+
+const increaseViewCount = async (req, res, next) => {
+  const { slug } = req.params;
+
+  try {
+    const blog = await Blog.findOne({ slug });
+    if (blog) {
+      blog.viewCount += 1;
+      await blog.save();
+    }
+  } catch (error) {
+    console.error("Error increasing view count:", error);
+  }
+
+  next();
+};
 
 app.post("/signup", postSignup);
 app.post("/login", postLogin);
 app.get("/blogs", getBlogs);
-app.post("/blogs", postBlogs);
-app.get("/blogs/:slug", getBlogsForSlug);
+app.get("/blogs/:slug", increaseViewCount, getBlogForSlug);
 
-const checkHeaderKey = (req, res, next) => {
-    const {api_token} = req.headers;
-    console.log("API Token:", api_token);
-
-    if(api_token == "admin") {
-        console.log("API Key valid");
-        next();
-
-    } else {
-        console.log("API Key invalid");
-        res.status(401).json({message: "Unauthorized"});
-    }
-};
-
-app.use(checkHeaderKey);
-
-app.get("/api/test1", (req, res) => {
-
-    console.log("Actual Controller Test1 called");
-    res.json({message: "Test1 route reached"});
-});
-
-app.get("/api/test2", (req, res) => {
-
-    console.log("Actual Controller Test2 called");
-    res.json({message: "Test2 route reached"});
-});
-
-// new ID based routes for fetching/updating by id
-import { getBlogById, updateBlogById } from "./controllers/blog.js";
-
-app.get('/blogs/id/:id', getBlogById);
-app.put('/blogs/id/:id', updateBlogById);
-
-
-
-
+app.post("/blogs", jwtCheck, postBlogs);
+app.patch("/blogs/:slug/publish", jwtCheck, patchPublishBlog);
+app.put("/blogs/:slug", jwtCheck, putBlogs);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
